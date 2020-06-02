d65193485df33ca09bb087ec868b7e80193542b1
Leo Bright <564312023@qq.com>
2020-06-02 16:00:09 +0800

    修改训练参数

--- examples/gat.py
+++ examples/gat.py
@@ -17,13 +17,13 @@ A, X, Y_train, Y_val, Y_test, idx_train, idx_val, idx_test = load_data('sanfranc
 N = X.shape[0]                # Number of nodes in the graph
 F = X.shape[1]                # Original feature dimension
 n_classes = Y_train.shape[1]  # Number of classes
-F_ = 8                        # Output size of first GraphAttention layer
-n_attn_heads = 8              # Number of attention heads in first GAT layer
-dropout_rate = 0.6            # Dropout rate (between and inside GAT layers)
+F_ = 4                        # Output size of first GraphAttention layer
+n_attn_heads = 4              # Number of attention heads in first GAT layer
+dropout_rate = 0            # Dropout rate (between and inside GAT layers)
 l2_reg = 5e-4/2               # Factor for l2 regularization
 learning_rate = 5e-3          # Learning rate for Adam
-epochs = 10000                # Number of training epochs
-es_patience = 100             # Patience fot early stopping
+epochs = 500                # Number of training epochs
+es_patience = 50             # Patience fot early stopping
 
 # Preprocessing operations
 X = preprocess_features(X)
